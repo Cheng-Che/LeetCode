81eb987e55dfbcfe0f051a08c28669580ce2349d
謝承哲 <cchsieh@iii.org.tw>
2017-04-24 15:33:59 +0800

    Initial Commit

---
+++ PracticeCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PracticeCode
+//
+//  Created by ccHsieh on 2017/4/24.
+//  Copyright © 2017年 ccHsieh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
