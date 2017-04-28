10f6216c3c997c6ad45eb7e93e3f9713682ed67f
謝承哲 <cchsieh@iii.org.tw>
2017-04-28 16:25:14 +0800

    first commit

---
+++ PracticeCode/RemoveDuplicate.swift
@@ -0,0 +1,35 @@
+//
+//  RemoveDuplicate.swift
+//  PracticeCode
+//
+//  Created by ccHsieh on 2017/4/28.
+//  Copyright © 2017年 ccHsieh. All rights reserved.
+//
+
+import Foundation
+
+class RemoveDuplicate {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+
+        var j = 1
+        for i in 0..<nums.count{
+            if i>0 && nums[i] != nums[i-1]{
+                nums[j] = nums[i]
+                j+=1
+            }
+        }
+        return j > nums.count ? nums.count : j
+    }
+    
+}
+
+/* Wrong answer :Memory Limit Exceeded
+ 
+ while i < nums.count{
+    while i+1 < nums.count && nums[i] == nums[i+1]{
+        nums.remove(at: i+1)
+    }
+    i += 1
+ }
+ 
+ */
--- PracticeCode/main.swift
+++ PracticeCode/main.swift
@@ -7,10 +7,16 @@
 //
 import Foundation
 
-let n = 5
+var n = [1,1,2, 3, 3, 5,7,7,7,7,8,8,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,10]
 
 
-let solution = CountAndSay()
-let s:String = solution.countAndSay(n)
+let solution = RemoveDuplicate()
+let index:Int = solution.removeDuplicates(&n)
+var ans:[Int] = []
 
-print(s)
+for i in 0..<index{
+    ans.append(n[i])
+}
+
+
+print(ans)
