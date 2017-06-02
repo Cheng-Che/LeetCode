1fc723cec044f8f2678d4387875a0a4f39df4aa8
謝承哲 <cchsieh@iii.org.tw>
2017-06-02 19:14:39 +0800

    Add count prime

--- PracticeCode/CountPrimes.swift
+++ PracticeCode/CountPrimes.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class countPrimes {
     func countPrimes(_ n: Int) -> Int {
-        
+        if n <= 2 { return 0 }
         var notPrime:[Bool] = []
         for i in 0..<n{
             if i%2 == 0 { notPrime.append(true) }
@@ -26,7 +26,6 @@ class countPrimes {
                     notPrime[num*j] = true
                     j+=2
                 }
-                
             }
             num += 2
             
@@ -35,19 +34,24 @@ class countPrimes {
     }
 }
 
-//var ans:[Int] = []
-//var num = 2
-//while num < n-1{
-//    var count = 0
-//    for i in 2..<Int(sqrt(Double(num)))+1{
-//        if num % i == 0{
-//            count += 1
-//        }
-//    }
-//    if count == 0{
-//        ans.append(num)
-//    }
-//    num+=1
-//}
-//print(ans)
-//return ans.count
+// func will time out
+
+/*
+var ans:[Int] = []
+var num = 2
+while num < n-1{
+    var count = 0
+    for i in 2..<Int(sqrt(Double(num)))+1{
+        if num % i == 0{
+            count += 1
+        }
+    }
+    if count == 0{
+        ans.append(num)
+    }
+    num+=1
+}
+print(ans)
+return ans.count
+
+*/
