f3fe4981c8604237733f4e3b5b62ef3a9d8c207d
謝承哲 <cchsieh@iii.org.tw>
2018-03-23 15:49:34 +0800

    Add FourSum2

--- PracticeCode/main.swift
+++ PracticeCode/main.swift
@@ -8,11 +8,14 @@
 import Foundation
 
 
-var input = [0,5,12,17,18,20, 1000, 2000]
+//var input = [0,5,12,17,18,20, 1000, 2000]
+var A = [1, 2]
+var B = [-2, -1]
+var C = [-1, 2]
+var D = [0, 2]
+
+let solution = FourSum2()
+let ans = solution.fourSumCount(A, B, C, D)
 
 
-let solution = FindPoisonedDuration()
-let ans = solution.findPoisonedDuration(input, 800)
-//
-//
 print(ans)
