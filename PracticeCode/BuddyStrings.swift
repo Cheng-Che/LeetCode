c54b745ce6940314bf2a97ffd593a5f9c756d471
謝承哲 <cchsieh@iii.org.tw>
2018-06-28 11:45:59 +0800

    Add BuddyStrings

---
+++ PracticeCode/BuddyStrings.swift
@@ -0,0 +1,62 @@
+//
+//  BuddyStrings.swift
+//  PracticeCode
+//
+//  Created by ccHsieh on 2018/6/27.
+//  Copyright © 2018年 ccHsieh. All rights reserved.
+//
+
+import Foundation
+
+class BuddyStrings {
+    func buddyStrings(_ A: String, _ B: String) -> Bool {
+        guard A.count == B.count else{ return false }
+        var dictA:[Character:Int] = [:]
+        var dictB:[Character:Int] = [:]
+        
+        for ch in A{
+            dictA[ch] = (dictA[ch] ?? 0) + 1
+        }
+        
+        for ch in B{
+            dictB[ch] = (dictB[ch] ?? 0) + 1
+        }
+        
+        guard dictA == dictB else{
+            return false
+        }
+        
+        if A == B{
+            for value in dictA.values{
+                if value > 1{
+                    return true
+                }
+            }
+            return false
+        }
+
+        var a = Array(A)
+        var b = Array(B)
+        var wordA:[Character] = []
+        var wordB:[Character] = []
+        
+        for i in 0..<a.count{
+            if a[i] != b[i]{
+                wordA.append(a[i])
+                wordB.append(b[i])
+            }
+            if wordA.count == 2{
+                if wordA == wordB.reversed(){
+                    wordA.removeAll()
+                    wordB.removeAll()
+                }else{
+                    wordA.removeLast()
+                    wordB.removeLast()
+                }
+            }
+        }
+        
+        return wordA.count == 0 ? true : false
+        
+    }
+}
--- PracticeCode/main.swift
+++ PracticeCode/main.swift
@@ -8,28 +8,12 @@
 import Foundation
 
 
-var input = [
-    [ 5, 1, 9,11],
-    [ 2, 4, 8,10],
-    [13, 3, 6, 7],
-    [15,14,12,16]
-]
+var input = "bahc"
+var input2 = "cagb"
 
-var input2 = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
 
-var input3 = [
-    [1,2,3,4,5],
-    [6,7,8,9,10],
-    [11,12,13,14,15],
-    [16,17,18,19,20],
-    [21,22,23,24,25]
-]
-let solution = RotateImage()
-let ans = solution.rotate(&input3)
+let solution = BuddyStrings()
+let ans = solution.buddyStrings(input, input2)
 
 print(ans)
 
