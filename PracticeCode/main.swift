4f431af83067d63feb49d2690da2f3509e03cc9b
謝承哲 <cchsieh@iii.org.tw>
2018-11-05 17:25:52 +0800

    edit input

--- PracticeCode/main.swift
+++ PracticeCode/main.swift
@@ -8,14 +8,12 @@
 import Foundation
 
 
-var input = ["bqum","dyccm","enjbor","ucjck","ruhqff","qtqqjaf","sux","stjqh","tilvgno","ljuxvof","pip","arwvwz","ccdf","fconiukv","yrsslnwo","yjxy","deejt","oemjbg","syqqcpow","mjmi","pxqnqvje","qfgd","rmhjtj","tttxcj","xaeclsji","noezki","rwnzxp","knicrcr","phuuya","bbdbsh","alqwor","fcuipqm","vpe","nyupge","gpbm","wcmtbayu","xswpnk","kety","xpmdke","uhgriwi","asu","mbf","flccmpzx","wtglzo","qydm","xrplf","dxl","ljee","mhfx","nij","pdczkbia","egjaov","jcbtlb","jbb","fckqcjub","fwu","laie","mmlutl","iahwokol","mfhibko","uwatxry","wczzt","mjrtn","woxjm","ins","gwsai","cdxorq","xfy","uzsjk","cgczfd","mktupl","dvgsnrtw","mdowr","tnylv","akewp","uwtdckg","nhe","jznax","ndhxh","jto","gvy","vuvwmzq","otyd","nwkbros","cuztmw","yvvtvf","vuqn","ehjospy","jnc","sie","mpbo","rkgiy","lcf","xdqxqsm","yeqeip","ybafxpxd","inzrzgw","komf","feqcn","quxhsl"]
-
-var input2 = "gioqujdqvf mphrpnzlu cocrrf jnxsfhmimtacvoywbffr iagrgfnkugkdnkz cndfdzkhwdeythfp ua pgjitcygbpsejny lffgcnr qwrcux sqifyzgkbgkdjqbmy awfohwmmbvzu xfxctl jtcbboxdhxfxrbrniaqk lxkdevf mubobc fmdyhiksmwolj fqfilsckzwqyip f ilnajedufkciqdgc ofxwycsf ufy kduadaxeptctlnrxs vhhlw wyowykidjjqi s dlndqxakelowvnspxer jxdghmniyw ilpxmipqmaygdwtmer nphonooy flj ogusvnnzucinquu cvjpxhyjsrxid yjzhw wghsdflltsjkmsc ckwsyw epwuhgeevvc qhy qowsfkddceklmaktaj smsspyopdibqkzjkpgjy vusnjicayeowdv qltktfhdfi sprcndsgtniyoigts bx epyelknmphjvkbjesfaz pipdjnvebarjpobza ue q urhfbtzruuqbca ndiqxxazyakpfe gcfordxaz czxnnuqfpbkjcxv nonkmghpxvyhr nwnnbttnoio ejkb pwwqqbyrf pz gejenqjkewug npivccuoklhtxulgp uqrh rbzuyjhyjzyoqrb uohkizufoawxbhbxxvpd nhoycstexfivggr occwowxjwhxtnfim pfdnlsnltaoiykr uozvcqjs zopyibfopkphrumvgd byacqluvmopfl oal daxhjlhumzvlgpxqctuj jct lmkysxgk nyevhrdiaiea flmtfmkjyf expokiuhwryeevhm bbsv srgpksxsoqguyyhrmyy dszgejq gtgwqvvtojn udbau pmavxyhoasyv ooudlxs bcjqkljkisjrnuvan sdutlauwdcopmvhmtepp msvxusiblcmgzbcjhe sqoylmmbsexlrspws zdzoiphk yszgacql dmpgwmcgfgtlaoeihi garvuq puxpkrongawlyqug qitwcgkahmgalz kiunuttlteofc jcziiw ecyqbfdbcjkbbvglwb xueqhtgwp aebsbsgayuizduk fisjeubcqxxiphklququ kkyacvywdjrsle ckd"
+var input = [[2,6,4],[8,7,6],[4,1,4]]
 
 
 
 
 
-let solution = ReplaceWords()
-let ans = solution.replaceWords(input, input2)
+let solution = MinFallingPathSum()
+let ans = solution.minFallingPathSum(input)
 print(ans)
